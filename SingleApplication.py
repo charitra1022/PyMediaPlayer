0a7bf93740df3b713111a0726fcd378a3c016215
Charitra Agarwal <charitraagrwal1022@gmail.com>
2020-11-17 12:45:25 +0530

    fixed server cleanup error in unix system

--- SingleApplication.py
+++ SingleApplication.py
@@ -8,6 +8,7 @@
 # Some modifications has been made based on Comments
 
 from PyQt5 import QtWidgets, QtCore, QtNetwork
+import sys
 
 
 # Check if an instance is already running, and then exit if found
@@ -37,10 +38,36 @@ class SingleApplicationWithMessaging(SingleApplication):
         super().__init__(argv, key)
         self._key = key
         self._timeout = 1000
-        self._server = QtNetwork.QLocalServer(self)
+
+        self._server = QtNetwork.QLocalServer(self)         # server object
+
         if not self.isRunning():
-            self._server.newConnection.connect(self.handleMessage)
-            self._server.listen(self._key)
+            if not self.try_connection():
+                if not self.reset_connection():
+                    QtCore.qDebug("Unknown error occurred... Exiting")
+                    sys.exit(1)
+
+
+    def try_connection(self):
+        """Try to connect to the Local Server"""
+        self._server.newConnection.connect(self.handleMessage)
+        state = self._server.listen(self._key)
+        if state:
+            QtCore.qDebug("Server connected.")
+        else:
+            QtCore.qDebug("Unable to start Server.")
+        return state
+
+    def reset_connection(self):
+        """Disconnect from existing server, and start a new one"""
+        QtCore.qDebug("Removing server..")
+        state = QtNetwork.QLocalServer.removeServer(self._key)
+        if state:
+            QtCore.qDebug("Remove Successful. Starting a new Server..")
+            state = self.try_connection()
+        else:
+            QtCore.qDebug("Cannot remove server.")
+        return state
 
     def handleMessage(self):
         """Accept the message from other instance and add it to shared memory for IPC"""
@@ -57,7 +84,7 @@ class SingleApplicationWithMessaging(SingleApplication):
             socket = QtNetwork.QLocalSocket(self)
             socket.connectToServer(self._key, QtCore.QIODevice.WriteOnly)
             if not socket.waitForConnected(self._timeout):
-                print(socket.errorString())
+                print("Inside sendMessage", socket.errorString())
                 return False
             if not isinstance(message, bytes):
                 message = message.encode('utf-8')
